9fa31cf367abb1af3de64ab8472e0e1d21d3cc30
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-15 20:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) return 0;
-    if (num === 1) return 1;
-
-    let a = 0;
-    let b = 1;
-
-    for (let i = 2; i <= num; i++) {
-        let next = a + b;
-        a = b;
-        b = next;
-    }
-
-    return b;
+	 if (num < 0) return null; 
+  if (num === 0) return 0;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+  
+  let a = 0, b = 1, fib = 1;
+  
+  for (let i = 3; i <= num; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+  
+  return fib;
 }
 
 module.exports = fibonacci;
